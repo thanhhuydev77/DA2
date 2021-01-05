8dc9ffbfee1aa714ea12986c319faf854bbdfc1c
thanhhuydev77 <thanhhuydev77@gmail.com>
2021-01-05 14:35:20 +0700

    5/1

---
+++ Model/ProductReview.go
@@ -0,0 +1,11 @@
+package Model
+
+type ProductReview struct {
+	Id             string `json:"id"`
+	Name           string `json:"name"`
+	ReviewUsername string `json: "reviews.username"`
+	Rating         int    `json:"	reviews.rating"`
+}
+
+type ProductReviewUserBased struct {
+}
